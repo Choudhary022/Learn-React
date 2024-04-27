2ff82b4c4f70007548aff6e7ee3a324e8c613cd8
Akash <chakashk@gmail.com>
2024-04-27 09:55:29 +0530

    Episode 3  is completed .

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ 
+const heading = <h1>
+    Namaste Java Script
+</h1>
+
+const root = ReactDOM.createRoot(document.getElementById('demo'));
+
+root.render(heading);
