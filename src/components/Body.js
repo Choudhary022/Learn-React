c72e4a10b2330d632c2aa393757a96b102cbed99
Akash <chakashk@gmail.com>
2024-05-06 21:49:47 +0530

    cross issue

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,31 @@
+import { useEffect } from "react";
+
+import { SWIGGY_API_URL } from "../utils/constants";
 
 import RestaurantCard from "./RestaurantCard";
 import RESTAURANT_LIST from "../utils/mockData";
 
+
 const Body = () => {
+
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      try {
+        const cros=require('cors');
+        const data = await fetch(SWIGGY_API_URL, );
+        const json = await data.json();
+        console.log("json format  : ", json);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+
+  }, [])
+
   return (
     <div className="body">
       <div className="search">
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,3 +4,4 @@ export const APP_LOGO_URL = "https://logos-world.net/wp-content/uploads/2023/03/
 export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 
+export const SWIGGY_API_URL="https://www.swiggy.com/api/seo/getListing?lat=17.4362456&lng=78.39062179999999"
